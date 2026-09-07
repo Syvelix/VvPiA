53362198abfe5ec05fd374b54ae948d86d1cfa4f
Syvelix <maksimysg0@gmail.com>
2026-09-07 21:27:21 +0300

    Major Basic.cpp improvments

---
+++ 1/Medium.cpp
@@ -0,0 +1,16 @@
+//Периметр и площадь квадрата
+
+//Использование директивы препроцессора
+#include <iostream>
+#include <iomanip>
+//Обращение к некоторым именам из стандартного пространства имён std
+using std::cout, std::cin, std::fixed, std::endl;
+using std::setprecision;
+
+//Точка входа
+int main()
+{
+    int side;
+    cout << "Введите длину стороны квадрата: ";
+    cin >> side;
+}
