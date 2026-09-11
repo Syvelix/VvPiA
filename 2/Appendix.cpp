5d7aa86ed5744545275923e9de55afdfdcf44512
Syvelix <maksimysg0@gmail.com>
2026-09-11 23:00:50 +0300

    2: Update Difficult + added test file

---
+++ 2/Appendix.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+using std::cin, std::cout;
+
+int main()
+{
+    float y = static_cast<float>(8.01111111122);
+    cout << y;
+}
--- 2/Difficult.cpp
+++ 2/Difficult.cpp
@@ -60,10 +60,18 @@ int main()
     num_d = get_double();
     num_f = get_float();
     num_i = get_int();
+    
+    //num_i = INT32_MAX;
+    //cout << "Целое число максимально\n";
+    //cout << format("Приведение INT32_MAX к double: {} --> {}\n\n", num_i, static_cast<double>(num_i));
+
+    //num_i = INT32_MIN;
+    //cout << "Целое число минимально\n";
+    //cout << format("Приведение INT32_MIN к double: {} --> {}\n\n", num_i, static_cast<double>(num_i));
 
     //Вывод максимума и минимума через соответствующие функции
     cout << format("\nМаксимальное из введённых чисел: {}\n", max({num_d, static_cast<double>(num_f), static_cast<double>(num_i)})) << endl;
-    cout << format("Минимальное из введённых чисел: {}\n", min({num_d, static_cast<double>(num_f), static_cast<double>(num_i)})) << endl;
+    cout << format("Минимальное из введённых чисел: {}\n\n", min({num_d, static_cast<double>(num_f), static_cast<double>(num_i)})) << endl;
 
     //Демонстрация потери точности
     cout << format("Демонстрация потери точности через приведение float к int: {} --> {}\n\n", num_f, static_cast<int>(num_f));
@@ -73,4 +81,6 @@ int main()
     //Демонстрация округления. Использовать при расчётах где допустимо округление в большую сторону
     //Явное приведение нужно тк round сам по себе возвращает не int
     cout << format("Округление double: {} --> {}\n\n", num_d, static_cast<int>((round(num_d))));
+
+    return 1;
 }
