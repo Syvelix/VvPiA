ed063961fa7b2834022424c6d8f2dc6da6853e02
Syvelix <maksimysg0@gmail.com>
2026-09-11 12:31:18 +0300

    2: Solved Medium level

---
+++ 2/Medium.cpp
@@ -0,0 +1,32 @@
+//Расстояние через время и скорость
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using std::cout, std::cin, std::fixed, std::setprecision, std::endl;
+
+int main()
+{
+    //Скорость возьму дробной, тк она - результат деления м на с
+    double speed;
+    //Время будет целое, секунды в этой программе - наименьшая единица времени
+    int time;
+
+    cout << "Введите скорость (м/с)\t";
+    cin >> speed;
+
+    cout << "Введите целое время (с)\t";
+    cin >> time;
+
+    // Точное расстояние, возможна унификация
+    double distance_dbl{time * speed};
+    // Округлённое расстояние, нет унификации из-за потери точности, явное преоброзование
+    int distance_int = static_cast<int>(round(time * speed));
+
+    //Вывод результатов
+    cout << "Расстояние с точностью до тысячных:\t" << fixed << setprecision(3) << distance_dbl << "м" << endl;
+    cout << "Округлённое расстояние:\t" << distance_int << "м" << endl;
+
+    return 0;
+}
