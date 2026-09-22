25c8fc70eadb700186a01e01fe1ae3ad34695197
Syvelix <maksimysg0@gmail.com>
2026-09-22 17:07:16 +0300

    Minor test, no overall changes

--- hollow_point/Legacy/test.cpp
+++ hollow_point/Legacy/test.cpp
@@ -1,14 +1,28 @@
-#include "Movie.hpp"
-#include "Seat.hpp"
-#include "Client.hpp"
-#include "Session.hpp"
 #include <iostream>
 #include <format>
 #include <filesystem>
 
-using std::cout, std::endl, std::format;
+using std::cout, std::cin, std::endl, std::format;
 
 int main()
 {
-    
+    unsigned i;
+    cin >> i;
+
+    switch(static_cast<int>(i))
+    {
+        case 1:
+            cout << "Понедельник";
+        case 2:
+            cout << "Вторник";
+        case 3:
+            cout << "Среда";
+        case 4:
+            cout << "Четверг";
+        default:
+            cout << "Не день";
+    }
+
+    cout << endl;
+    return 0;
 }
